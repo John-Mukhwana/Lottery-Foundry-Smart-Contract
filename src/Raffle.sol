e2ff02122a1a104abbf4a2a7fef4293f656ef198
John-Mukhwana <johnbradill67@gmail.com>
2025-08-16 21:52:41 +0300

    style: enhance code comments for better readability and structure in Raffle contract

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,29 @@
+// Layout of the contract file:
+// version
+// imports
+// errors
+// interfaces, libraries, contract
+
+// Inside Contract:
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+
+
+
 
 //SPDX-License-Identifier:MIT
 
@@ -37,7 +63,7 @@ contract Raffle{
        return i_entranceFee;
    }
 
-   
+
 
 
 }
