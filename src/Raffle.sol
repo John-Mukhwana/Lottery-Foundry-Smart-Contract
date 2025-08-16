297d8505bfb5884167999ff5a28fa1d0774a362f
John-Mukhwana <johnbradill67@gmail.com>
2025-08-16 21:25:01 +0300

    update Raffle contract documentation to indicate ongoing development

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -3,4 +3,13 @@
 
 pragma solidity 0.8.30;
 
+
+/**
+ * @title Raffle contract
+ * @author cypherpulse.base.eth
+ * @notice This contract implements a simple raffle system.
+ * @dev implements Chainlink VRFv2.5
+ */
+
+
 contract Raffle{}
