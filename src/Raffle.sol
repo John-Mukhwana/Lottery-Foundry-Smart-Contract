ab5b0d5a17e7e681c5b51910f8f39673a01f6f3a
John-Mukhwana <johnbradill67@gmail.com>
2025-08-17 22:24:06 +0300

    fix: add missing semicolon in enterRaffle function

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -62,6 +62,7 @@ contract Raffle{
         
         if(msg.value < i_entranceFee) revert Raffle_NotEnoughEthSent();
         s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender)
     }
 
     function pickWinner() public {}
