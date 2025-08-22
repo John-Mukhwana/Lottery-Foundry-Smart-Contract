2ce29bb64c17ad9cda33ca88505bf9e161a5988a
John-Mukhwana <johnbradill67@gmail.com>
2025-08-22 05:25:12 +0300

    fix: add transfer logic to fulfillRandomWords for recent winner payout

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -114,6 +114,8 @@ contract Raffle is VRFConsumerBaseV2Plus {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
        s_recentWinner = recentWinner;
+       (bool success,) = recentWinner.call{value: address(this).balance}("");
+       
     }
 
     /**
