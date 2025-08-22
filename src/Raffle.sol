4d8376d8cd07283f06d4c85ae9f3ea35918ec4f8
John-Mukhwana <johnbradill67@gmail.com>
2025-08-22 05:48:45 +0300

    fix: reset players array after selecting a winner in pickWinner function

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -134,7 +134,9 @@ contract Raffle is VRFConsumerBaseV2Plus {
         s_recentWinner = recentWinner;
         s_raffleState = RaffleState.OPEN;
 
-        
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+
        (bool success,) = recentWinner.call{value: address(this).balance}("");
        if(!success){
         revert Raffle__TransferFailed();
