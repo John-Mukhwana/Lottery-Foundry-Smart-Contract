c0c7da57ce788c59d843e304f2e002b07d41218f
John-Mukhwana <johnbradill67@gmail.com>
2025-08-24 14:42:30 +0300

    fix: add comment clarification for interval in getSepoliaEthConfig function and implement getLocalConfig function in HelperConfig contract

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -21,12 +21,23 @@ contract HelperConfig is Script {
         function getSepoliaEthConfig() public pure returns(NetworkConfig memory){
             return NetworkConfig({
                 entranceFee: 0.01 ether,//1e16
-                interval: 30,//
+                interval: 30,// 30 seconds
                 vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
                 gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
                 callbackGasLimit: 500000,
                 subscriptionId: 0
             })
         }
+    }
+
+    function getLocalConfig() public pure returns(NetworkConfig memory){
+        return NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30, //30 seconds
+            vrfCoordinator: address(0),
+            gasLane: "",
+            callbackGasLimit: 500000,
+            subscriptionId: 0
+        })
     }
 }
