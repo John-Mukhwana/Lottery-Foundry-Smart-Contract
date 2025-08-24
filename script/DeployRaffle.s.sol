732b2f376840b904c26444a81cefe4357aaf384d
John-Mukhwana <johnbradill67@gmail.com>
2025-08-24 14:24:24 +0300

    feat: add deployContract function to DeployRaffle contract

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -5,6 +5,9 @@ pragma solidity 0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 
+
 contract DeployRaffle is Script {
     function run() public{}
+
+    function deployContract public returns(Raffle,HelperConfig){}
 }
