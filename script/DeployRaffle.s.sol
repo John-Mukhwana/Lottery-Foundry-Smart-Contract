7bd0a274fedc8f42bb2d1c4341e89641743657b7
John-Mukhwana <johnbradill67@gmail.com>
2025-08-24 14:26:57 +0300

    fix: correct import statement for Raffle contract in DeployRaffle script

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,7 +4,7 @@
 pragma solidity 0.8.19;
 
 import {Script} from "forge-std/Script.sol";
-
+import {Raffle} from "../src/Raffle.sol";
 
 contract DeployRaffle is Script {
     function run() public{}
