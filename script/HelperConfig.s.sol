701ec537f92463865802fec09cc8eb85ac4de65c
John-Mukhwana <johnbradill67@gmail.com>
2025-08-25 21:22:24 +0300

    fix: add missing import for VRFCoordinatorV2_5Mock in HelperConfig contract

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,6 +3,7 @@
 pragma solidity 0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 
 abstract contract CodeConstants {
     uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
@@ -73,5 +74,5 @@ contract HelperConfig is CodeConstants, Script {
     }
 
     //Deploy Mocks and such
-    
+
 }
