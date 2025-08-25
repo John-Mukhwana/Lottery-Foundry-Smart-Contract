33f401c92d1a271d23db68551727bb44d2c16518
John-Mukhwana <johnbradill67@gmail.com>
2025-08-25 21:20:59 +0300

    fix: improve formatting and consistency in HelperConfig contract

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,16 +2,17 @@
 
 pragma solidity 0.8.19;
 
-import {Script} from "forge-std/Script.sol";abstract contract CodeConstants {
+import {Script} from "forge-std/Script.sol";
+
+abstract contract CodeConstants {
     uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 public constant LOCAL_CHAIN_ID = 31337;
     uint256 public constant BASE_SEPOLIA_CHAIN_ID = 84531;
 }
 
-
 contract HelperConfig is CodeConstants, Script {
     error HelperConfig__InvalidChainId();
-    struct NetworkConfig{
+    struct NetworkConfig {
         uint256 entranceFee;
         uint256 interval;
         address vrfCoordinator;
@@ -19,52 +20,58 @@ contract HelperConfig is CodeConstants, Script {
         uint32 callbackGasLimit;
         uint256 subscriptionId;
     }
-    
+
     NetworkConfig public localNetworkConfig;
     mapping(uint256 => NetworkConfig) public networkConfigs;
-    
+
     constructor() {
-     networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
-     networkConfigs[BASE_SEPOLIA_CHAIN_ID] = getBaseSepoliaEthConfig();
+        networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
+        networkConfigs[BASE_SEPOLIA_CHAIN_ID] = getBaseSepoliaEthConfig();
     }
 
-    function getConfigByChainId(uint256 chainId) public returns (NetworkConfig memory) {
-    if (networkConfigs[chainId].vrfCoordinator != address(0)) {
-        return networkConfigs[chainId];
-    } else if (chainId == LOCAL_CHAIN_ID) {
-        return getOrCreateAnvilEthConfig();
-    } else {
-        revert HelperConfig__InvalidChainId();
+    function getConfigByChainId(
+        uint256 chainId
+    ) public returns (NetworkConfig memory) {
+        if (networkConfigs[chainId].vrfCoordinator != address(0)) {
+            return networkConfigs[chainId];
+        } else if (chainId == LOCAL_CHAIN_ID) {
+            return getOrCreateAnvilEthConfig();
+        } else {
+            revert HelperConfig__InvalidChainId();
+        }
     }
-    
-    }
-    function getSepoliaEthConfig() public pure returns(NetworkConfig memory){
-            return NetworkConfig({
-                entranceFee: 0.01 ether,//1e16
-                interval: 30,// 30 seconds
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        return
+            NetworkConfig({
+                entranceFee: 0.01 ether, //1e16
+                interval: 30, // 30 seconds
                 vrfCoordinator: 0x5C210eF41CD1a72de73bF76eC39637bB0d3d7BEE,
                 gasLane: 0x9e1344a1247c8a1785d0a4681a27152bffdb43666ae5bf7d14d24a5efd44bf71,
                 callbackGasLimit: 500000,
                 subscriptionId: 0
             });
-        }
-    
-
-    function getLocalConfig() public pure returns(NetworkConfig memory){
-        return NetworkConfig({
-            entranceFee: 0.01 ether,
-            interval: 30, //30 seconds
-            vrfCoordinator: address(0),
-            gasLane: "",
-            callbackGasLimit: 500000,
-            subscriptionId: 0
-        });
     }
 
-  function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
-    // Check to see if we set an active network localNetworkConfig
-    if (localNetworkConfig.vrfCoordinator != address(0)) {
-        return localNetworkConfig;
- }  
-}
+    function getLocalConfig() public pure returns (NetworkConfig memory) {
+        return
+            NetworkConfig({
+                entranceFee: 0.01 ether,
+                interval: 30, //30 seconds
+                vrfCoordinator: address(0),
+                gasLane: "",
+                callbackGasLimit: 500000,
+                subscriptionId: 0
+            });
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        // Check to see if we set an active network localNetworkConfig
+        if (localNetworkConfig.vrfCoordinator != address(0)) {
+            return localNetworkConfig;
+        }
+    }
+
+    //Deploy Mocks and such
+    
 }
