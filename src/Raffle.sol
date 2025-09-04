797528b9f73f67fec38d36ef3e294cf15bc30805
John-Mukhwana <johnbradill67@gmail.com>
2025-09-04 07:21:57 +0300

    Add getPlayer function to retrieve player addresses from the raffle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -199,7 +199,8 @@ contract Raffle is VRFConsumerBaseV2Plus {
     function getRaffleState() external view returns (RaffleState) {
         return s_raffleState;
     }
-
+    
+    //Function to get players 
     function getPlayer(uint256 indexOfPlayer) external view returns (address) {
         return s_players[indexOfPlayer];
     }
