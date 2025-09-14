5a0e8834b8f396698e12a3c98796d89baab1d6d2
John-Mukhwana <johnbradill67@gmail.com>
2025-09-14 23:21:16 +0300

    Fix typo in run function declaration in CreateSubscription contract

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -4,5 +4,10 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 
 contract CreateSubscription is Script {
+    function createSubscriptionUsingConfig() public returns (uint64) {
 
+    }
+    function run() pulic{
+        
+    }
 }
