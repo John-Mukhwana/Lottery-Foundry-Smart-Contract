dd3ca28a68a08754e01f9afcd33f01d9e70c6a11
John-Mukhwana <johnbradill67@gmail.com>
2025-09-14 23:33:53 +0300

    Add import statement for HelperConfig in CreateSubscription contract

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract CreateSubscription is Script {
     function createSubscriptionUsingConfig() public returns (uint64) {
-
+        HelperConfig helperConfig = new HelperConfig();
     }
     function run() external returns (uint64) {
         return createSubscriptionUsingConfig();n 
