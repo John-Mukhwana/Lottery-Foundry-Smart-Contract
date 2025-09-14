190cf047705ec380f311a6374db9a52a036f59e2
John-Mukhwana <johnbradill67@gmail.com>
2025-09-14 23:35:08 +0300

    Update createSubscriptionUsingConfig to return vrfCoordinator from HelperConfig

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -7,9 +7,9 @@ import {HelperConfig} from "./HelperConfig.s.sol";
 contract CreateSubscription is Script {
     function createSubscriptionUsingConfig() public returns (uint64) {
         HelperConfig helperConfig = new HelperConfig();
-        helperConfig.getConfig();
+        helperConfig.getConfig().vrfCoordinator;
     }
     function run() external returns (uint64) {
-        return createSubscriptionUsingConfig();n 
+        return createSubscriptionUsingConfig(); 
     }
 }
