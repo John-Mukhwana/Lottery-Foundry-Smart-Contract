d5d7f8564ad5d2dc4e6ec4e23de628df60892639
John-Mukhwana <johnbradill67@gmail.com>
2025-09-24 17:35:12 +0300

    Add test to ensure performUpkeep reverts when checkUpkeep is false

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -123,5 +123,20 @@ contract RaffleTest is Test {
     raffle.performUpkeep("");
 }
 
-
+function testPerformUpkeepRevertsIfCheckUpkeepIsFalse() public {
+    // Arrange
+    uint256 currentBalance = 0;
+    uint256 numPlayers = 0;
+    Raffle.RaffleState rState = raffle.getRaffleState();
+    // Act / Assert
+    vm.expectRevert(
+        abi.encodeWithSelector(
+            Raffle.Raffle__UpkeepNotNeeded.selector,
+            currentBalance,
+            numPlayers,
+            rState
+        )
+    );
+    raffle.performUpkeep("");
+}
 }
