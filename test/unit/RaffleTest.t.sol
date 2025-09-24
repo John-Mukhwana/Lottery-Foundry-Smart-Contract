d4503700dc9fba659eed08b68094676a56090a49
John-Mukhwana <johnbradill67@gmail.com>
2025-09-24 17:18:50 +0300

    Fix test function name and add checkUpkeep test for non-open raffle state

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -86,10 +86,23 @@ contract RaffleTest is Test {
     }
 
     //check upkeep //
-    function testCheckUpKeepReturnsFalseIfNoBalance() public {
+    function testCheckUpkeepReturnsFalseIfNoBalance() public {
         // Arrange
         vm.warp(block.timestamp + interval + 1);
         vm.roll(block.number + 1);
+        // Act
+        (bool upkeepNeeded, ) = raffle.checkUpkeep("");
+        // Assert
+        assert(!upkeepNeeded);
+    }
+
+    function testCheckUpkeepReturnsFalseIfRaffleNotOpen() public {
+        // Arrange
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+        raffle.performUpkeep("");
         // Act
         (bool upkeepNeeded, ) = raffle.checkUpkeep("");
         // Assert
